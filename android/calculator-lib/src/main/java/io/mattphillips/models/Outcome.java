c6f8b6672f9f9aa64344218be8260afe5456bdd5
Matt Phillips <mattphillips1016@gmail.com>
2016-03-13 03:38:04 +0000

    Added models to represent the outcome of a bet

---
+++ android/calculator-lib/src/main/java/io/mattphillips/models/Outcome.java
@@ -0,0 +1,28 @@
+package io.mattphillips.models;
+
+import java.math.BigDecimal;
+
+public class Outcome {
+
+    private final Result result;
+    private final BigDecimal payout;
+    private final BigDecimal profit;
+
+    public Outcome(final Result result, final BigDecimal payout, final BigDecimal profit) {
+        this.result = result;
+        this.payout = payout;
+        this.profit = profit;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public BigDecimal getPayout() {
+        return payout;
+    }
+
+    public BigDecimal getProfit() {
+        return profit;
+    }
+}
---
+++ android/calculator-lib/src/main/java/io/mattphillips/models/Result.java
@@ -0,0 +1,7 @@
+package io.mattphillips.models;
+
+public enum Result {
+    WIN,
+    DRAW,
+    LOSE;
+}
