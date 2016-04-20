5e21d98ad0d09720244824f0772a2c59babaeeaf
Matt Phillips <mattphillips1016@gmail.com>
2016-04-20 12:21:21 +0100

    added utility to determine if team is home or away

--- android/calculator-lib/src/main/java/io/mattphillips/models/Team.java
+++ android/calculator-lib/src/main/java/io/mattphillips/models/Team.java
@@ -3,4 +3,12 @@ package io.mattphillips.models;
 public enum Team {
     HOME,
     AWAY;
+
+    public boolean isHome() {
+        return this == HOME;
+    }
+
+    public boolean isAway() {
+        return this == AWAY;
+    }
 }
