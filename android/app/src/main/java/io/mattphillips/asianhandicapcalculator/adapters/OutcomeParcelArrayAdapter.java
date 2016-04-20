244e97b6d3c811c09f66bb405ec9da5731e418c2
Matt Phillips <mattphillips1016@gmail.com>
2016-04-20 16:50:44 +0100

    added bet to be displayed as part of outcome

--- android/app/src/main/java/io/mattphillips/asianhandicapcalculator/adapters/OutcomeParcelArrayAdapter.java
+++ android/app/src/main/java/io/mattphillips/asianhandicapcalculator/adapters/OutcomeParcelArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -17,9 +18,18 @@ public class OutcomeParcelArrayAdapter extends ArrayAdapter<OutcomeParcel> {
     private final Context context;
     private final OutcomeParcel[] values;
 
+    @Bind(R.id.team) TextView team;
+    @Bind(R.id.odds) TextView odds;
+    @Bind(R.id.handicap) TextView handicap;
+    @Bind(R.id.stake) TextView stake;
+    @Bind(R.id.score) TextView score;
+
     @Bind(R.id.result) TextView result;
     @Bind(R.id.payout) TextView payout;
     @Bind(R.id.profit) TextView profit;
+
+    @Bind(R.id.outcomeHeader) TextView outcomeHeader;
+    @Bind(R.id.outcomeContainer) LinearLayout outcomeContainer;
 
     public OutcomeParcelArrayAdapter(Context context, OutcomeParcel[] values) {
         super(context, R.layout.list_item_outcome, values);
@@ -35,10 +45,30 @@ public class OutcomeParcelArrayAdapter extends ArrayAdapter<OutcomeParcel> {
         ButterKnife.bind(this, rowView);
 
         OutcomeParcel outcome = values[position];
+
+        team.setText(outcome.getTeam());
+        odds.setText(outcome.getOdds());
+        handicap.setText(outcome.getHandicap());
+        stake.setText(outcome.getStake());
+        score.setText(outcome.getScore());
+
         result.setText(outcome.getResult());
         payout.setText(outcome.getPayout());
         profit.setText(outcome.getProfit());
 
+        if (outcome.getResult().equalsIgnoreCase("WIN")) {
+            updateOutcomeColour(R.color.colorWin);
+        } else if (outcome.getResult().equalsIgnoreCase("LOSE")) {
+            updateOutcomeColour(R.color.colorLose);
+        } else {
+            updateOutcomeColour(R.color.colorDraw);
+        }
+
         return rowView;
+    }
+
+    private void updateOutcomeColour(int colorRes) {
+        outcomeHeader.setBackgroundColor(getContext().getResources().getColor(colorRes));
+        outcomeContainer.setBackgroundColor(getContext().getResources().getColor(colorRes));
     }
 }
