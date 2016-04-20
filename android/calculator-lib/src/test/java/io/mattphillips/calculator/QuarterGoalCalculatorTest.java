c41faad83710e7ffeee9796f514bf8545e2756d9
Matt Phillips <mattphillips1016@gmail.com>
2016-04-20 12:30:26 +0100

    added utility to build all scenario bets

--- android/calculator-lib/src/test/java/io/mattphillips/calculator/QuarterGoalCalculatorTest.java
+++ android/calculator-lib/src/test/java/io/mattphillips/calculator/QuarterGoalCalculatorTest.java
@@ -3,6 +3,7 @@ package io.mattphillips.calculator;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import io.mattphillips.models.Bet;
 import io.mattphillips.models.Outcome;
@@ -17,6 +18,75 @@ import static io.mattphillips.models.Team.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class QuarterGoalCalculatorTest {
+
+    @Test
+    public void shouldBuildAllScenarioBetsCorrectlyForHomeBackedHandicap() {
+        // TODO: remove this null should be optional or another constructor
+        Bet bet = new Bet(Team.HOME, new Odds("1"), new Handicap("+2.75"), new Stake("10"), null);
+        List<Bet> bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(0, 4));
+        assertThat(bets).contains(bet.adjustScore(0, 2));
+        assertThat(bets).contains(bet.adjustScore(0, 3));
+
+        bet = new Bet(Team.HOME, new Odds("1"), new Handicap("+2.25"), new Stake("10"), null);
+        bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(0, 1));
+        assertThat(bets).contains(bet.adjustScore(0, 2));
+        assertThat(bets).contains(bet.adjustScore(0, 3));
+    }
+
+    @Test
+    public void shouldBuildAllScenarioBetsCorrectlyForHomeLaidHandicap() {
+        Bet bet = new Bet(Team.HOME, new Odds("1"), new Handicap("-2.75"), new Stake("10"), null);
+        List<Bet> bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(4, 0));
+        assertThat(bets).contains(bet.adjustScore(2, 0));
+        assertThat(bets).contains(bet.adjustScore(3, 0));
+
+        bet = new Bet(Team.HOME, new Odds("1"), new Handicap("-2.25"), new Stake("10"), null);
+        bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(1, 0));
+        assertThat(bets).contains(bet.adjustScore(2, 0));
+        assertThat(bets).contains(bet.adjustScore(3, 0));
+    }
+
+    @Test
+    public void shouldBuildAllScenarioBetsCorrectlyForAwayBackedHandicap() {
+        Bet bet = new Bet(Team.AWAY, new Odds("1"), new Handicap("+2.75"), new Stake("10"), null);
+        List<Bet> bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(4, 0));
+        assertThat(bets).contains(bet.adjustScore(2, 0));
+        assertThat(bets).contains(bet.adjustScore(3, 0));
+
+        bet = new Bet(Team.AWAY, new Odds("1"), new Handicap("+2.25"), new Stake("10"), null);
+        bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(1, 0));
+        assertThat(bets).contains(bet.adjustScore(2, 0));
+        assertThat(bets).contains(bet.adjustScore(3, 0));
+    }
+
+    @Test
+    public void shouldBuildAllScenarioBetsCorrectlyForAwayLaidHandicap() {
+        Bet bet = new Bet(Team.AWAY, new Odds("1"), new Handicap("-2.75"), new Stake("10"), null);
+        List<Bet> bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(0, 4));
+        assertThat(bets).contains(bet.adjustScore(0, 2));
+        assertThat(bets).contains(bet.adjustScore(0, 3));
+
+        bet = new Bet(Team.AWAY, new Odds("1"), new Handicap("-2.25"), new Stake("10"), null);
+        bets = QuarterGoalCalculator.buildQuarterGoalAllScenariosBets(bet);
+        assertThat(bets.size()).isEqualTo(3);
+        assertThat(bets).contains(bet.adjustScore(0, 1));
+        assertThat(bets).contains(bet.adjustScore(0, 2));
+        assertThat(bets).contains(bet.adjustScore(0, 3));
+    }
 
     @Test
     public void shouldDetermineOutcomeAsWinWhenBothFullGoalAndHalfGoalBetsAreWinners() throws Exception {
